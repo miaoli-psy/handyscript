1adfc4d4e5c7eea82404a4c09982c5289a742d94
miaoli-psy <chuoli223@yahoo.com>
2021-12-11 14:56:52 +0100

    generate a list of continue strings

--- gen_list_names.py
+++ gen_list_names.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    save_to_csv = False
-    my_range = 31
-    my_old_str = "stims/sub"
+    save_to_csv = True
+    start_range = 10
+    end_range = 18
+    my_old_str = "stims/whitesub"
     my_suffix = ".png"
 
-    my_str_list = [my_old_str+str(i)+my_suffix for i in range(1, my_range)]
+    my_str_list = [my_old_str+str(i)+my_suffix for i in range(start_range, end_range + 1)]
 
     df = pd.DataFrame(my_str_list)
 
